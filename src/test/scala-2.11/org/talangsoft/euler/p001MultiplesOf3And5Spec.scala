6515202887e90a183c203b0fbc5d1f5e8fa2798b
Tamas Lang <tamas.lang@talangsoft.org>
2015-11-23 14:18:34 +0000

    initial commit + solution of the first problem

---
+++ src/test/scala-2.11/org/talangsoft/euler/p001MultiplesOf3And5Spec.scala
@@ -0,0 +1,17 @@
+package org.talangsoft.euler
+
+import org.scalatest.{ShouldMatchers, WordSpec}
+
+class p001MultiplesOf3And5Spec extends WordSpec with ShouldMatchers{
+  "multiplesOf3And5" should {
+    "return 23 for numbers below 10" in {
+      p001MultiplesOf3And5.multiplesOf3And5(1,9) shouldBe 23
+    }
+  }
+
+  "multiplesOf3And5" should {
+    "return ? for numbers below 1000" in {
+      p001MultiplesOf3And5.multiplesOf3And5(1,999) shouldBe 233168
+    }
+  }
+}
