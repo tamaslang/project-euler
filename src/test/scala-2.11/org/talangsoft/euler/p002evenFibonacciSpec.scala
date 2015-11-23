922fe7491eb0000448422e5ed20337f2201350ce
Tamas Lang <tamas.lang@talangsoft.org>
2015-11-23 20:55:02 +0000

    add solution for problem 2:

---
+++ src/test/scala-2.11/org/talangsoft/euler/p002evenFibonacciSpec.scala
@@ -0,0 +1,18 @@
+package org.talangsoft.euler
+
+import org.scalatest.{ShouldMatchers, WordSpec}
+
+class p002evenFibonacciSpec extends WordSpec with ShouldMatchers {
+  "fibonacci" should {
+    "return fibonacci till n" in {
+      p002evenFibonacci.fibonacci(100) shouldBe List(1, 2, 3, 5, 8, 13, 21, 34, 55, 89)
+    }
+  }
+
+  "evenFibonacciSum" should {
+    "return ? for fibonacci till 4000000" in {
+      p002evenFibonacci.evenFibonacciSum(4000000) shouldBe 4613732
+    }
+  }
+
+}
